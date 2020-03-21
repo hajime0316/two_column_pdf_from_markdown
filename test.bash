f1bbb1c115885444eb4ac6aa7f3549305e72667e
Hajime <hajime0316@s.okayama-u.ac.jp>
2020-03-22 01:10:39 +0900

    Add while statment

--- test.bash
+++ test.bash
@@ -6,4 +6,9 @@ header_flag=0
 # sed -E s/'^<\s*?div.*?("author"|"date")\s*?>'/'% '/ test.md
 # sed -E s@'<\s*?/div\s*?>'@@ test.md
 
-cat test.md | sed -E s/'^\# +'/'% '/ | sed -E s/'^<\s*?div.*?("author"|"date")\s*?>'/'% '/ | sed -E s@'<\s*?/div\s*?>'@@
+line_num=1
+
+cat test.md | sed -E s/'^\# +'/'% '/ | sed -E s/'^<\s*?div.*?("author"|"date")\s*?>'/'% '/ | sed -E s@'<\s*?/div\s*?>'@@ |
+    while read line; do
+        echo "$line"
+    done
