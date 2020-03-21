7ce925f0fa5c50f5ce14443c6a521b13b9432919
Hajime <hajime0316@s.okayama-u.ac.jp>
2020-03-21 18:43:16 +0900

    Conect sed commands

--- test.bash
+++ test.bash
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-local header_flag=0
+header_flag=0
 
-sed -E s/'^\# +'/'% '/ test.md
-sed -E s/'^<\s*?div.*?("author"|"date")\s*?>'/'% '/ test.md
-sed -E s@'<\s*?/div\s*?>'@@ test.md
+# sed -E s/'^\# +'/'% '/ test.md
+# sed -E s/'^<\s*?div.*?("author"|"date")\s*?>'/'% '/ test.md
+# sed -E s@'<\s*?/div\s*?>'@@ test.md
+
+cat test.md | sed -E s/'^\# +'/'% '/ | sed -E s/'^<\s*?div.*?("author"|"date")\s*?>'/'% '/ | sed -E s@'<\s*?/div\s*?>'@@
