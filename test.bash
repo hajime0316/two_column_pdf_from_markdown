41ac4b3bc0e1902f890ad4a13dde13bee323b17d
Hajime <hajime0316@s.okayama-u.ac.jp>
2020-03-24 20:26:32 +0900

    Functionalization

--- test.bash
+++ test.bash
@@ -1,19 +1,23 @@
 #!/bin/bash
 
-body_flag=0
+make_header() {
+    local body_flag=0
 
-cat "${1:?No input file}" | while read line; do
-    if [ "$body_flag" == 0 ]; then
-        if [[ $line =~ ^\<div\ +class\ *=\ *\"date\"\ *\> ]]; then
-            body_flag=1
+    cat "${1:?No input file}" | while read line; do
+        if [ "$body_flag" == 0 ]; then
+            if [[ $line =~ ^\<div\ +class\ *=\ *\"date\"\ *\> ]]; then
+                body_flag=1
+            fi
+            echo "$line" |
+                sed -E -n '/(^\# +|^<div +class *= *("author"|"date") *>)/p' |
+                sed -E 's/^\# +/% /' |
+                sed -E 's/^<div +class *= *("author"|"date") *>/% /' |
+                sed -E 's@</div *>@@'
+        else
+            echo "$line" |
+                sed -E 's/^#//'
         fi
-        echo "$line" |
-            sed -E -n '/(^\# +|^<div +class *= *("author"|"date") *>)/p' |
-            sed -E 's/^\# +/% /' |
-            sed -E 's/^<div +class *= *("author"|"date") *>/% /' |
-            sed -E 's@</div *>@@'
-    else
-        echo "$line" |
-        sed -E 's/^#//'
-    fi
-done
+    done
+}
+
+make_header "$1"
