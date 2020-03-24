320cd8eb211777c34c023bdad78c13f7ebbcc154
Hajime <hajime0316@s.okayama-u.ac.jp>
2020-03-24 19:50:41 +0900

    Remove unnecessary lines

--- test.bash
+++ test.bash
@@ -1,18 +1,6 @@
 #!/bin/bash
 
-header_flag=0
 body_flag=0
-
-# sed -E s/'^\# +'/'% '/ test.md
-# sed -E s/'^<\s*?div.*?("author"|"date")\s*?>'/'% '/ test.md
-# sed -E s@'<\s*?/div\s*?>'@@ test.md
-
-line_num=1
-
-# cat test.md | sed -E s/'^\# +'/'% '/ | sed -E s/'^<\s*?div.*?("author"|"date")\s*?>'/'% '/ | sed -E s@'<\s*?/div\s*?>'@@ |
-#     while read line; do
-#         echo "$line"
-#     done
 
 cat test.md | while read line; do
     if [ "$body_flag" == 0 ]; then
