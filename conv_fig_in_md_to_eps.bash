f9ce13d72df457b612e017ac3a9a03659cfd40cb
Hajime <hajime0316@s.okayama-u.ac.jp>
2020-03-28 17:52:21 +0900

    Implement conv_to_eps()

--- conv_fig_in_md_to_eps.bash
+++ conv_fig_in_md_to_eps.bash
@@ -3,8 +3,6 @@
 # .mdファイルをスクレイピングして
 # 変換すべきファイルのPATHを取得
 
-
-
 # bmepsコマンドで画像をepsに変換
 # 引数は1つ（file）
 # 入力ファイル名に.epsを付けたものを
@@ -12,7 +10,9 @@
 
 conv_to_eps() {
     input_file="${1:?No input file}"
-    input_file_dir="${input_file%/*}"
-    input_file_name="${input_file##*/}"
-    input_file_name_without_extention="${input_file_name%.*}"
+    input_file_without_extention="${input_file%.*}"
+
+    bmeps -c "$input_file" "$input_file_without_extention.eps"
 }
+
+conv_to_eps "$1"
