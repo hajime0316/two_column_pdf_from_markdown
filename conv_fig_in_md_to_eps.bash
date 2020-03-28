d5bfcebc25849d57f3296b233a84f8f81286de69
Hajime <hajime0316@s.okayama-u.ac.jp>
2020-03-28 20:31:38 +0900

    Remove unnecessary function

--- conv_fig_in_md_to_eps.bash
+++ conv_fig_in_md_to_eps.bash
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-# .mdファイルをスクレイピングして
-# 変換すべきファイルのPATHを取得
-
-scrape_fig_in_md() {
-    local input_file="${1:?No input file}"
-    cat "$input_file" | sed -E -n '/!\[.*\]\(.*\.(JPG|jpg|JPEG|jpeg|PNG|png)\)/p' | sed -E 's/^.*!\[.*\]\((.*?)\).*$/\1/'
-}
-
 input_file="${1:?No input file}"
 if [[ ! $input_file =~ ^\./.*$ ]]; then
     input_file="./$input_file"
@@ -15,4 +7,8 @@ fi
 
 input_file_dir="${input_file%/*}"
 
-scrape_fig_in_md "$input_file" | sed s@^@$input_file_dir/@ | xargs -n1 ./conv_to_eps.bash
+cat "$input_file" |
+    sed -E -n '/!\[.*\]\(.*\.(JPG|jpg|JPEG|jpeg|PNG|png)\)/p' |
+    sed -E 's/^.*!\[.*\]\((.*?)\).*$/\1/' |
+    sed s@^@$input_file_dir/@ |
+    xargs -n1 ./conv_to_eps.bash
