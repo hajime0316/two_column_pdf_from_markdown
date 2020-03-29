917f053c6e70e6f130c2c4e53508405baa7500d2
Hajime <hajime0316@s.okayama-u.ac.jp>
2020-03-29 16:55:43 +0900

    Fix output_dir

--- pandoc_md2pdf.bash
+++ pandoc_md2pdf.bash
@@ -21,10 +21,14 @@ make_header() {
     done
 }
 
-# TODO: デフォルトではoutput_dir = input_file_dirとする
-output_dir="${2:-.}"
 input_file="${1:?No input file}"
+if [[ ! $input_file =~ ^\./.*$ ]]; then
+    input_file="./$input_file"
+fi
+input_file_dir="${input_file%/*}"
 input_file_name="${input_file##*/}"
 input_file_name_without_extension="${input_file_name%.*}"
+
+output_dir="${2:-$input_file_dir}"
 
 make_header "$input_file" | pandoc --template=mytemplate.tex -s -f markdown-auto_identifiers -t latex -o "$output_dir/$input_file_name_without_extension.tex"
